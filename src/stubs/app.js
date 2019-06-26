d95aab8ec6f23c5070eff0f02356c46e69ddb894
Luis Miguel Alvarado <luismiguel1730@gmail.com>
2019-06-26 14:58:39 -0400

    remove styled components example

--- src/stubs/app.js
+++ src/stubs/app.js
@@ -1,33 +1,14 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes React and other helpers. It's a great starting point while
- * building robust, powerful web applications using React + Laravel.
- */
 require('./bootstrap');
 
 import React from 'react';
 import { render } from 'react-dom';
-import styled from 'styled-components';
-
-const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
-`;
-
-const Wrapper = styled.section`
-  padding: 4em;
-  background: papayawhip;
-`;
 
 export default function App() {
-	return (
-		<Wrapper>
-			<Title>
-				Hello World!
-			</Title>
-		</Wrapper>
-	);
+    return (
+        <div>
+	  <h1>Hello World!</h1>
+	</div>
+    );
 }
 
 if (document.getElementById('example')) {
